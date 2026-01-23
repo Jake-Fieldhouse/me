563d3bc84adb7ccae9be69d64b8c95671972db64
Jke <jke.contact.me@gmail.com>
2026-01-23 15:39:12 +0000

    fix: restore HTMLAttributes import

--- src/components/FluidCursor.vue
+++ src/components/FluidCursor.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref } from "vue";
+import { onMounted, onUnmounted, ref, type HTMLAttributes } from "vue";
 
 interface ColorRGB {
   r: number;
