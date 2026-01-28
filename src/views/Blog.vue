80a9522e6a7cf914f565a27313594f7da59ce83a
Jke <jke.contact.me@gmail.com>
2026-01-28 12:01:07 +0000

    fix: remove unused imports in Blog.vue

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed, onMounted } from 'vue'
+import { ref } from 'vue'
 import { useScrollReveal } from '../composables/useScrollReveal'
 
 const heroRef = ref<HTMLElement | null>(null)
